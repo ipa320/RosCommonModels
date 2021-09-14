ebd74a9bd2b68d5244a987a9b293a722517ba806
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2021-09-14 08:25:00 +0200

    add useful the results from the large scale analysis for the noetic distro

---
+++ rosdistro/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path=$1
+count_success=0
+count=0
+
+for f in $path/*.ros
+do
+  ((count=count+1))
+  number_of_lines=$(wc -l $f)
+  if(( ${number_of_lines%% *} > 5 ))
+    then
+      ((count_success=count_success+1))
+  else
+    rm $f
+  fi
+done
+
+echo "Total packages:" $count
+echo "Total obtained models:" $count_success
+echo "Rate:" $((count_success*100/count))"%"
